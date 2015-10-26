09b20f1e0d0f7ad3ba12d8d47e30f96d9e264944
gomesuit <gomesuit@gmail.com>
2015-10-26 17:57:00 +0900

    add free form

--- src/main/java/jansible/model/Parameters.java
+++ src/main/java/jansible/model/Parameters.java
@@ -5,14 +5,25 @@ import java.util.List;
 
 public class Parameters {
 	private List<Parameter> pList = new ArrayList<>();
+	private String freeForm;
 
 	public void addParameter(Parameter param){
 		pList.add(param);
 	}
 
+	public void setFreeForm(String freeForm) {
+		this.freeForm = freeForm;
+	}
+
 	@Override
 	public String toString() {
 		StringBuffer result = new StringBuffer();
+		
+		if(freeForm != null){
+			result.append(freeForm);
+			result.append(" ");
+		}
+		
 		for(Parameter param : pList){
 			result.append(param);
 			result.append(" ");
