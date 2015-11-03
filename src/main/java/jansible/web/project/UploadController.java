0baf3cb5bffe82c4d9ae316c12e7c1c06552f699
gomesuit <gomesuit@gmail.com>
2015-11-04 00:16:24 +0900

    sample upload

--- src/main/java/jansible/web/project/UploadController.java
+++ src/main/java/jansible/web/project/UploadController.java
@@ -1,13 +1,16 @@
 package jansible.web.project;
 
+import jansible.web.project.form.TemplateUploadForm;
+
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,14 +18,17 @@ import org.springframework.web.multipart.MultipartFile;
 public class UploadController {
 
 	@RequestMapping(value = "/uploadform", method = RequestMethod.GET)
-	public String provideUploadInfo() {
+	public String provideUploadInfo(Model model) {
+		TemplateUploadForm templateUploadForm = new TemplateUploadForm();
+		model.addAttribute("templateUploadForm", templateUploadForm);
 		return "upload";
 	}
 
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
 	public @ResponseBody String handleFileUpload(
-			@RequestParam("name") String name,
-			@RequestParam("file") MultipartFile file) {
+			@ModelAttribute TemplateUploadForm form) {
+		String name = form.getName();
+		MultipartFile file = form.getFile();
 		if (!file.isEmpty()) {
 			try {
 				byte[] bytes = file.getBytes();
---
+++ src/main/java/jansible/web/project/form/TemplateUploadForm.java
@@ -0,0 +1,21 @@
+package jansible.web.project.form;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class TemplateUploadForm {
+	private String name;
+	private MultipartFile file;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public MultipartFile getFile() {
+		return file;
+	}
+	public void setFile(MultipartFile file) {
+		this.file = file;
+	}
+}
