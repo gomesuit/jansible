6e356a74fa1d80fa512409d2b2dd426baa886831
gomesuit <gomesuit@gmail.com>
2015-11-11 00:04:14 +0900

    jenkins job

---
+++ etc/jenkins.sh
@@ -0,0 +1,11 @@
+rm -rf $projectName
+git clone $repositoryUrl $projectName
+cd $projectName
+#git pull
+ansible-playbook -i hosts $groupName.yml --syntax-check
+ansible-playbook -i hosts $groupName.yml --list-tasks
+ansible-playbook -i hosts $groupName.yml --list-hosts
+ansible-playbook -i hosts $groupName.yml --check
+echo $projectName
+echo $repositoryUrl
+echo $groupName
